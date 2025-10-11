2c0e339987c0edfadbf13e4dea2008c6812184ac
Leaa46 <leabekteshu1@gmail.com>
2025-10-11 16:50:33 +0200

    projekti

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+    <p class="copyright">Lea Bekteshi</p>
+</footer>
+</div>
+<?php wp_footer();?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+function wp_adv_theme_support()
+{
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+
+}
+
+add_action('after_setup_theme', 'wp_adv_theme_support');
+
+function wp_adv_menus(){
+    $locations = [
+        'primary' =>'Desktop Primary Left Sidebar',
+        'footer' => 'Footer Menu Items',
+    ];
+    register_nav_menus($locations);
+
+}
+
+add_action('init', 'wp_adv_menus');
+
+function wp_adv_register_styles(){
+    wp_enqueue_style('wp-adv-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', [], '5.0.2', 'all');
+    wp_enqueue_style('wp_adv_style', get_stylesheet_uri(), ['wp_adv_bootstrap'], '1.0', 'all');
+     wp_enqueue_style( 'wp_adv_fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css', [], '7.0.1', 'all');
+     }
+     add_action('wp_enqueue_scripts', 'wp_adv_register_styles');
+
