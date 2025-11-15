70f17305d26bdf8da2aba72de1ec09ef56389711
leaa46 <leabekteshi1@gmail.com>
2025-11-15 16:51:53 +0100

    df

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<article class="content px-3 py-5 p-md-5">
+    <h1 class="page-title">404 - page Not Found</h1>
+    <p>Sorry, the page zou are looking for does not exist. Please use the search from below ton find what zou are loking for.</p>
+<?php get_search_form();?>
+</article>
+<?php get_footer(); ?> 
